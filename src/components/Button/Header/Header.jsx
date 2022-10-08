de8a7b5092e166ba8b81ee3dc61f4827e58841ea
VladGridin <vldgrdn@gmail.com>
2022-10-08 09:30:49 +0300

    test2

---
+++ src/components/Button/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className={'header'}>
+            <Button></Button>
+        </div>
+    );
+};
+export default Header;

daa82076a390ee564c5007fbc1435d7b106ddb3f
VladGridin <vldgrdn@gmail.com>
2022-10-08 10:11:41 +0300

    sdsddfdsd

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
-  const {onToogleButton} = useTelegram
+   const {tg} = useTelegram
 
   // useEffect( () => {
   //     tg.ready();
   // }, [])
-
+  const onToogleButton = () => {
+    if(tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.show();
+    }
+  }
   return (
     <div className="App">
       Hello

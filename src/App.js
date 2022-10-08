58254cddf0f5a7696b5126ad45263cb41d3c254e
VladGridin <vldgrdn@gmail.com>
2022-10-08 09:44:39 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 
 function App() {
+  const {onTogleButton, tg} = useTelegram
 
   useEffect( () => {
       tg.ready();
   }, [])
 
-
-
   return (
     <div className="App">
-      work
-
+      <button onClick={onTogleButton}>toogle</button>
     </div>
   );
 }
